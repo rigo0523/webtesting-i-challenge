18aa323f68e66bd25200de3070f9dc96e577a344
Rigo <limon1285@gmail.com>
2021-02-16 23:13:01 -0600

    updated

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -10,11 +10,19 @@ function success(item) {
 }
 
 function fail(item) {
+  if (item.enhancement < 10) {
+    item.durability -= 5;
+  } else if (item.enhancement === 15) {
+    item.durability += 5;
+  } else {
+    item.enhancement -= 1; // enhacement = enhancement - 1;
+    item.durability -= 10;
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,26 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+test("Initial Test", () => {
+  expect(true).toBe(true);
+});
+
+const olympus = {
+  name: "Zeus",
+  durability: 99,
+  enhancement: 19,
+};
+
+describe("test repair", () => {
+  test("it should test Zeus durability to 100", () => {
+    expect(() => {
+      enhancer.repair(olympus.durability).toBe(olympus.durability === 100);
+    });
+  });
+});
+
+describe("fail", () => {
+  it("it will decrease durability by 5 if enhancement level is under 15", () => {
+    const expected = { name: "Zeus", durability: 99, enhancement: 11 };
+    expect(enhancer.fail(expected)).toBe(olympus);
+  });
+});
